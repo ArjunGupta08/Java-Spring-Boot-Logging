b7750221475321bcfa0a1195b00d40ea5364a926
Arjun Gupta <arjungupta0817@gmail.com>
2024-01-02 00:40:14 +0530

    1) - SLF4J, 2) - USING LOGBACK

--- src/main/java/com/ag/springBootLogging/controller/Employee.java
+++ src/main/java/com/ag/springBootLogging/controller/Employee.java
@@ -14,8 +14,22 @@ public class Employee {
     private static final Logger loginInfo = LoggerFactory.getLogger(Employee.class);
 
     @GetMapping(value = "{empId}")
-    public String getEmployee(@PathVariable String empId){
+    public String getEmployee(@PathVariable String empId) {
         loginInfo.info("Logger enabled");
+
+       /*
+        THERE ARE TWO TYPES OF LOGGING SYSTEM AVAILABLE : ->
+        1) - USING SLF4J
+        2) - USING LOGBACK
+
+         If (U r using slf4j) {
+             Use application.yaml to enable debug Log
+         } else if (U r using LogBack) {
+             Use logback.xml to enable debug Log
+             logback create a full logs file.
+         }*/
+
+        loginInfo.debug("Debug enabled");
         return "Employee h.";
     }
 
