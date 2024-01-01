dc82c4744464d77a019218b41edd6c978f5a9bcf
Arjun Gupta <arjungupta0817@gmail.com>
2024-01-01 23:51:10 +0530

    Initial commit

---
+++ src/test/java/com/ag/springBootLogging/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.springBootLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
